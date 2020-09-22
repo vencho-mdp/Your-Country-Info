fd5c9c77ef12b6ec8ee5076f78997c13fc554262
unusuariocomunpez <65968861+unusuariocomunpez@users.noreply.github.com>
2020-09-22 03:16:55 -0300

    First Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <input style="color: blue;" type="text" v-model="countryName" @keyup="searchData" />
+    <!--Change this to a Select Tag :)-->
+    <input style="color: red; " type="text" v-model="search" @keyup="searchData" />
+    <span v-if="errorMessage">Error: We can't find what you are looking for :(</span>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      countryName: undefined,
+      search: undefined,
+      errorMessage: false
+    };
+  },
+  methods: {
+    searchData() {
+      let search = this.search.toLowerCase();
+      let countryName =
+        this.countryName.charAt(0).toUpperCase() + this.countryName.slice(1);
+      if (search && countryName) {
+        axios
+          .get("https://restcountries.eu/rest/v2/name/" + countryName)
+          .then(function(response) {
+            let fetchResponse = response.data[0];
+            let img = fetchResponse.flag;
+            if (fetchResponse[search] !== undefined) {
+              document.write(
+                fetchResponse[search] + `<img src="${img}"> </img>`
+              );
+              console.log(response.status); // ex.: 200
+            } else {
+              this.errorMessage = true;
+            }
+          })
+          .catch(error => {
+            console.log(error);
+            this.errorMessage = true;
+          });
+      }
+    }
+  },
+  computed: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
